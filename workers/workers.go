202491078aef1c834a371169bded83a87908a37d
André Martins <andresmartins910@gmail.com>
2024-01-20 16:38:38 -0300

    workers and task structure

---
+++ workers/workers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"app/task"
+	"log"
+
+	"github.com/hibiken/asynq"
+)
+
+func main() {
+	srv := asynq.NewServer(
+		asynq.RedisClientOpt{Addr: "localhost:6379"},
+		asynq.Config{Concurrency: 10},
+	)
+
+	mux := asynq.NewServeMux()
+	mux.HandleFunc(task.TypeWelcomeEmail, task.HandleWelcomeEmailTask)
+	mux.HandleFunc(task.TypeReminderEmail, task.HandleReminderEmailTask)
+
+	if err := srv.Run(mux); err != nil {
+		log.Fatal(err)
+	}
+}
